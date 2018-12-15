b1e11647df542df6d8e591cb933d4e272869592a
Niklas Mollenhauer <nikeee@outlook.com>
2018-12-15 04:42:44 +0100

    Move margin struct

--- src/HolzShots.Core.Tests/Properties/AssemblyInfo.cs
+++ src/HolzShots.Core.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0")]
 
-[assembly: AssemblyInformationalVersion(LibraryInformation.FullVersionString)]
-
 [assembly: InternalsVisibleTo("HolzShots.Core.Tests")]
-
