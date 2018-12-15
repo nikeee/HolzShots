2458bb3c371a784891059e05ff515f63397bfbc6
Niklas Mollenhauer <nikeee@outlook.com>
2018-12-15 04:37:30 +0100

    Add some rule suppression

---
+++ src/HolzShots/GlobalSuppressions.vb
@@ -0,0 +1,1 @@
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification:="<Pending>", Scope:="member", Target:="~P:HolzShots.UI.Dialogs.UploadResultWindow.DisplayUrl")>
