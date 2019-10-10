bcc89b682f3fdba4583849c2e86fef98fcc46038
Niklas Mollenhauer <nikeee@outlook.com>
2019-10-11 00:42:59 +0200

    Fix visibility

--- src/HolzShots.Common/StringExtensions.cs
+++ src/HolzShots.Common/StringExtensions.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 namespace HolzShots.Common
 {
-    internal static class StringExtensions
+    public static class StringExtensions
     {
         private static readonly char[] _illegalFileNameChars = Path.GetInvalidFileNameChars();
         public static string SanitizeFileName(this string fileName) => SanitizeFileName(fileName, null);
