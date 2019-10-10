ab6aca930bae0f6e795551506a2b66ef68f78aaf
Niklas Mollenhauer <nikeee@outlook.com>
2019-10-11 00:22:39 +0200

    Move all of HolzShots.UI to HolzShots

--- src/HolzShots/Interop/NativeMethods.vb
+++ src/HolzShots/Interop/NativeMethods.vb
@@ -9,6 +9,7 @@ Namespace Interop
         Private Const User32 As String = "user32.dll"
         Private Const Shell32 As String = "shell32.dll"
         Private Const DwmApi As String = "dwmapi.dll"
+        Private Const UxTheme As String = "Uxtheme.dll"
 
 #Region "shell32"
 
@@ -26,6 +27,30 @@ Namespace Interop
         <DllImport(DwmApi, PreserveSig:=False)>
         Friend Shared Function DwmIsCompositionEnabled() As <MarshalAs(UnmanagedType.U1)> Boolean
         End Function
+
+        <DllImport(DwmApi)>
+        Public Shared Function DwmDefWindowProc(ByVal hWnd As IntPtr, ByVal msg As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr, ByRef plResult As IntPtr) As Integer
+        End Function
+
+        <DllImport(DwmApi, PreserveSig:=True)>
+        Public Shared Function DwmExtendFrameIntoClientArea(ByVal hwnd As IntPtr, ByRef margins As Common.NativeTypes.Margin) As Integer
+        End Function
+
+        <DllImport(DwmApi, EntryPoint:="#104")>
+        Public Shared Function DwmSetColorizationColor(ByVal colorizationColor As Integer, ByVal colorizationOpaqueBlend As Boolean, ByVal opacity As Integer) As Integer
+        End Function
+
+        <DllImport(DwmApi, PreserveSig:=False)>
+        Public Shared Sub DwmGetColorizationColor(ByRef colorizationColor As Integer, <MarshalAs(UnmanagedType.Bool)> ByRef colorizationOpaqueBlend As Boolean)
+        End Sub
+
+        <DllImport(DwmApi, EntryPoint:="#127", CharSet:=Runtime.InteropServices.CharSet.Ansi, SetLastError:=True, ExactSpelling:=True)>
+        Public Shared Sub DwmGetColorizationParameters(ByRef parameters As NativeTypes.DwmColorizationParams)
+        End Sub
+
+        <DllImport(DwmApi, EntryPoint:="#131", CharSet:=Runtime.InteropServices.CharSet.Ansi, SetLastError:=True, ExactSpelling:=True)>
+        Public Shared Sub DwmSetColorizationParameters(ByRef parameters As NativeTypes.DwmColorizationParams)
+        End Sub
 
 #End Region
 #Region "user32"
@@ -137,6 +162,10 @@ Namespace Interop
         Friend Shared Function GetClassName(ByVal hWnd As IntPtr, ByVal lpClassName As StringBuilder, ByVal nMaxCount As Integer) As Integer
         End Function
 
+        <DllImport(User32)>
+        Public Shared Function FlashWindowEx(ByRef pwfi As NativeTypes.FlashWindowInfo) As <MarshalAs(UnmanagedType.Bool)> Boolean
+        End Function
+
 #End Region
 #Region "Drawing-Related"
 
@@ -166,6 +195,20 @@ Namespace Interop
 #End Region
 
 #End Region
+#Region "ustheme"
+
+        <DllImport(UxTheme)>
+        Public Shared Function SetWindowThemeAttribute(ByVal hWnd As IntPtr, ByVal wtype As Integer, ByRef attributes As NativeTypes.WtaOptions, ByVal size As UInteger) As Integer
+        End Function
+        <DllImport(UxTheme)>
+        Public Shared Function GetThemeMargins(hTheme As IntPtr, hdc As IntPtr, iPartId As Integer, iStateId As Integer, iPropId As Integer, rect As IntPtr, ByRef pMargins As Common.NativeTypes.Margin) As Integer
+        End Function
+        <DllImport(UxTheme, ExactSpelling:=True, CharSet:=CharSet.Unicode)>
+        Public Shared Function SetWindowTheme(ByVal hWnd As IntPtr, ByVal pszSubAppName As String, ByVal pszSubIdList As Integer) As Integer
+        End Function
+
+#End Region
+
 
     End Class
 End Namespace
