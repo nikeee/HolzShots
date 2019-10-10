3627f91c0bf1035b97299e3e9e486f691a7249bd
Niklas Mollenhauer <nikeee@outlook.com>
2019-10-11 00:35:03 +0200

    CI does not support throw expressions yet

--- src/HolzShots.Common/ReflectionUtil.cs
+++ src/HolzShots.Common/ReflectionUtil.cs
@@ -36,7 +36,7 @@ namespace HolzShots.Common
             if (field == null)
                 throw new ArgumentException();
 
-            return field.SetValue(instance, value);
+            field.SetValue(instance, value);
         }
     }
 }
