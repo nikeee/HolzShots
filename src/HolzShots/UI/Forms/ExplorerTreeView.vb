1c3fa40a9cb99ad3c30332b1f018f00e865af350
Niklas Mollenhauer <nikeee@outlook.com>
2020-04-14 01:44:38 +0200

    Migrate remaining types to native library

--- src/HolzShots/UI/Forms/ExplorerTreeView.vb
+++ src/HolzShots/UI/Forms/ExplorerTreeView.vb
@@ -10,7 +10,7 @@ Namespace Windows.Forms
             Get
                 Dim cp As CreateParams = MyBase.CreateParams
                 If EnvironmentEx.IsVistaOrHigher AndAlso Not Me.Scrollable Then
-                    cp.Style = cp.Style Or Interop.NativeTypes.Tv.NoHScroll
+                    cp.Style = cp.Style Or Native.WindowStyleFlags.TVS_NoHScroll
                 End If
 
                 Return cp
@@ -20,11 +20,11 @@ Namespace Windows.Forms
         Protected Overrides Sub OnHandleCreated(ByVal e As EventArgs)
             MyBase.OnHandleCreated(e)
             If EnvironmentEx.IsVistaOrHigher Then
-                Dim hndl As IntPtr = Native.User32.SendMessage(Handle, Native.WindowMessage.TVM_GetExtendedStyle, IntPtr.Zero, IntPtr.Zero)
+                Dim extendedStyleIntPtr = Native.User32.SendMessage(Handle, Native.WindowMessage.TVM_GetExtendedStyle, IntPtr.Zero, IntPtr.Zero)
 
-                hndl = New IntPtr(hndl.ToInt32 Or Interop.NativeTypes.Tv.ExAutoSHcroll Or Interop.NativeTypes.Tv.ExFaceInOutExpandOs)
+                Dim extendedStyle = extendedStyleIntPtr.ToInt32() Or Native.ExtendedWindowStyleFlags.TVS_EX_AutoHScroll Or Native.ExtendedWindowStyleFlags.TVS_EX_FadeInOutExpandOs
 
-                Native.User32.SendMessage(Handle, Native.WindowMessage.TVM_SetExtendedStyle, IntPtr.Zero, hndl)
+                Native.User32.SendMessage(Handle, Native.WindowMessage.TVM_SetExtendedStyle, IntPtr.Zero, New IntPtr(extendedStyle))
 
                 Dim unused = Native.UxTheme.SetWindowTheme(Handle, "explorer", 0)
             End If
