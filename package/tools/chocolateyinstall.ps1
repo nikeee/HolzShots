d30d1ae3fc7937265452fcc3e18dbf3aa631c218
Niklas Mollenhauer <nikeee@outlook.com>
2020-04-13 03:02:39 +0200

    Add artifact publishing for GitHub

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -4,6 +4,6 @@ $packageName = $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://github.com/nikeee/HolzShots/releases/download/v__REPLACE_VERSION__/HolzShots.zip'
 $checksum = '__REPLACE_CHECKSUM__'
-$checksumType = 'sha265'
+$checksumType = 'sha256'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir -checkSum $checksum -checksumType $checksumType
