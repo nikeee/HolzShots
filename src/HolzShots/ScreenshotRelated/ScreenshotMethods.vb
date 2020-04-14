3e2fa7c281658bd676ff3d6eb700e5867d012646
Niklas Mollenhauer <nikeee@outlook.com>
2020-04-14 02:25:04 +0200

    Clean up screenshot creation in legacy code

--- src/HolzShots/ScreenshotRelated/ScreenshotMethods.vb
+++ src/HolzShots/ScreenshotRelated/ScreenshotMethods.vb
@@ -78,41 +78,44 @@ Namespace ScreenshotRelated
 
         Private Shared Function DoAeroOn(wndHandle As IntPtr, includeMargin As Boolean, smallMargin As Boolean) As WindowScreenshotSet
 
-            Dim rct As HolzShots.Native.Rect
-            Dim plc As Native.User32.WindowPlacement
-            Native.User32.GetWindowRect(wndHandle, rct)
-            Native.User32.GetWindowPlacement(wndHandle, plc)
+            Dim nativeRectangle As HolzShots.Native.Rect
+            Native.User32.GetWindowRect(wndHandle, nativeRectangle)
+
+            Dim placement As Native.User32.WindowPlacement
+            Native.User32.GetWindowPlacement(wndHandle, placement)
 
             If includeMargin Then
-                If plc.showCmd <> 3 Then
-                    Dim left = rct.Left - If(smallMargin, 4, 17)
-                    Dim top = rct.Top - If(smallMargin, 4, 17)
-                    Dim right = rct.Right + If(smallMargin, 4, 21)
-                    Dim bottom = rct.Bottom + If(smallMargin, 4, 21)
+                If placement.showCmd <> 3 Then
+                    Dim left = nativeRectangle.Left - If(smallMargin, 4, 17)
+                    Dim top = nativeRectangle.Top - If(smallMargin, 4, 17)
+                    Dim right = nativeRectangle.Right + If(smallMargin, 4, 21)
+                    Dim bottom = nativeRectangle.Bottom + If(smallMargin, 4, 21)
 
-                    rct = New Native.Rect(
+                    nativeRectangle = New Native.Rect(
                         Math.Max(left, SystemInformation.VirtualScreen.Left),
                         Math.Max(top, SystemInformation.VirtualScreen.Top),
                         Math.Min(right, SystemInformation.VirtualScreen.Right),
                         Math.Min(bottom, SystemInformation.VirtualScreen.Bottom)
                     )
                 Else
-                    Dim tmprect As Rectangle = rct 'rct.ToRectangle()
+                    Dim tmprect As Rectangle = nativeRectangle
                     Dim center As New Point(tmprect.X + CInt(tmprect.Width / 2), tmprect.Y + CInt(tmprect.Height / 2))
-                    rct = Screen.GetWorkingArea(center) 'NativeTypes.Rect.FromRectangle(Screen.GetWorkingArea(center))
+                    nativeRectangle = Screen.GetWorkingArea(center) 'NativeTypes.Rect.FromRectangle(Screen.GetWorkingArea(center))
                 End If
             End If
 
-            Dim nrct As Rectangle = Rectangle.FromLTRB(rct.Left, rct.Top, rct.Right, rct.Bottom)
-            Dim curp As New Point(Cursor.Position.X - nrct.Location.X, Cursor.Position.Y - nrct.Location.Y)
+            Dim drawingRectangle As Rectangle = nativeRectangle
 
-            If nrct.Size.Height < 0 OrElse nrct.Size.Width < 0 Then Return Nothing
+            If drawingRectangle.Size.Height < 0 OrElse drawingRectangle.Size.Width < 0 Then Return Nothing
 
-            Using bg As New BackgroundForm(nrct.Location, nrct.Size) _
+            Dim cursorPositonOnScreenshot As New Point(Cursor.Position.X - drawingRectangle.Location.X, Cursor.Position.Y - drawingRectangle.Location.Y)
+
+
+            Using bg As New BackgroundForm(drawingRectangle.Location, drawingRectangle.Size) _
                 'New Point(rct.Left, rct.Top), New Size(rct.Right - rct.Left, rct.Bottom - rct.Top))
                 'Using bg As New FloatingWindow(nrct.X, nrct.Y, nrct.Width, nrct.Height)
-                Using bmpBlack As New Bitmap(nrct.Width, nrct.Height, PixelFormat.Format32bppPArgb)
-                    Using bmpWhite As New Bitmap(nrct.Width, nrct.Height, PixelFormat.Format32bppPArgb)
+                Using bmpBlack As New Bitmap(drawingRectangle.Width, drawingRectangle.Height, PixelFormat.Format32bppPArgb)
+                    Using bmpWhite As New Bitmap(drawingRectangle.Width, drawingRectangle.Height, PixelFormat.Format32bppPArgb)
 
                         bg.Visible = True
 
@@ -123,14 +126,14 @@ Namespace ScreenshotRelated
 
                         Using ga As Graphics = Graphics.FromImage(bmpBlack)
                             ga.CompositingQuality = CompositingQuality.HighQuality
-                            ga.CopyFromScreen(nrct.X, nrct.Y, 0, 0, bg.Size)
+                            ga.CopyFromScreen(drawingRectangle.X, drawingRectangle.Y, 0, 0, bg.Size)
                         End Using
 
                         bg.BackColor = Color.White
                         bg.Refresh()
 
                         Using ga As Graphics = Graphics.FromImage(bmpWhite)
-                            ga.CopyFromScreen(nrct.X, nrct.Y, 0, 0, bg.Size)
+                            ga.CopyFromScreen(drawingRectangle.X, drawingRectangle.Y, 0, 0, bg.Size)
                         End Using
 
                         ScreenshotMethodsHelper.StartRedraw(wndHandle)
@@ -147,31 +150,25 @@ Namespace ScreenshotRelated
                         Dim windowTitle = ScreenshotMethodsHelper.GetWindowTitle(wndHandle)
                         Dim processName = ScreenshotMethodsHelper.GetProcessNameOfWindow(wndHandle)
 
-                        Return New WindowScreenshotSet(result, curp, windowTitle, processName)
+                        Return New WindowScreenshotSet(result, cursorPositonOnScreenshot, windowTitle, processName)
                     End Using
                 End Using
             End Using
         End Function
 
         Private Shared Function DoAeroOff(wndHandle As IntPtr) As WindowScreenshotSet
-            Dim rct As Native.Rect
-            Native.User32.GetWindowRect(wndHandle, rct)
+            Dim nativeRectangle As Native.Rect
+            Native.User32.GetWindowRect(wndHandle, nativeRectangle)
+            Dim drawingRectangle As Rectangle = nativeRectangle
 
-            Dim nrct As Rectangle = Rectangle.FromLTRB(rct.Left, rct.Top, rct.Right, rct.Bottom)
+            Dim cursorPositonOnScreenshot As New Point(Cursor.Position.X - drawingRectangle.Location.X, Cursor.Position.Y - drawingRectangle.Location.Y)
 
-            Dim curp As New Point(Cursor.Position.X - nrct.Location.X, Cursor.Position.Y - nrct.Location.Y)
-            Dim bmp As New Bitmap(nrct.Width, nrct.Height)
-
-            Using g As Graphics = Graphics.FromImage(bmp)
-                g.Clear(Color.FromArgb(255, 13, 11, 12))
-                g.CopyFromScreen(nrct.Location.X, nrct.Location.Y, 0, 0, nrct.Size)
-                g.Flush()
-            End Using
+            Dim bmp = ScreenshotCreator.CaptureScreenshot(drawingRectangle)
 
             Dim windowTitle = ScreenshotMethodsHelper.GetWindowTitle(wndHandle)
             Dim processName = ScreenshotMethodsHelper.GetProcessNameOfWindow(wndHandle)
 
-            Return New WindowScreenshotSet(bmp, curp, windowTitle, processName)
+            Return New WindowScreenshotSet(bmp, cursorPositonOnScreenshot, windowTitle, processName)
         End Function
 
         Protected Shared Function CaptureCursor() As Bitmap
