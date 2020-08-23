b80be5264c88c2bcf7ca16e34ebc329a7cedca1f
Niklas Mollenhauer <nikeee@outlook.com>
2020-08-23 02:15:33 +0200

    Check if ten or higher

--- src/HolzShots.Windows/Forms/EnvironmentEx.cs
+++ src/HolzShots.Windows/Forms/EnvironmentEx.cs
@@ -7,9 +7,13 @@ namespace HolzShots.Windows.Forms
     {
         public static bool IsVistaOrHigher => Environment.OSVersion.Version.Major >= 6;
         public static bool IsSevenOrHigher => (Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor >= 1) || Environment.OSVersion.Version.Major > 6;
+        public static bool IsTenOrHigher => Environment.OSVersion.Version.Major >= 10;
 
         public static bool AppsUseLightTheme()
         {
+            if (!IsTenOrHigher)
+                return false;
+
             var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize");
             return (int)key.GetValue("AppsUseLightTheme", 1) != 0;
         }
