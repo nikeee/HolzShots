71a2a0568a4005b22930731bc76986eb1363a00c
Niklas Mollenhauer <nikeee@outlook.com>
2020-09-07 22:56:43 +0200

    Migrate tests to new project format

--- src/HolzShots.Core.Tests/Properties/AssemblyInfo.cs
+++ src/HolzShots.Core.Tests/Properties/AssemblyInfo.cs
@@ -3,16 +3,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("HolzShots.Core.Tests")]
-[assembly: AssemblyProduct("HolzShots.Core.Tests")]
-[assembly: AssemblyDescription("HolzShots Core Component Tests")]
-
 [assembly: Guid("56abb0f5-9c8b-4bc9-9d16-bc9598657b5d")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
-
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
 
 [assembly: InternalsVisibleTo("HolzShots.Core.Tests")]
