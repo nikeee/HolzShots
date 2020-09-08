076c0a28b73a743e7c3b0d686df2162b01d43224
Niklas Mollenhauer <nikeee@outlook.com>
2020-09-08 14:52:26 +0200

    Ditch Microsoft.WindowsAPICodePack.Dialogs
    ...and use new Windows Forms TaskDialog instead.
    See: https://github.com/dotnet/winforms/issues/146
    Addresses #34

--- src/HolzShots/Interop/HumanInterop.vb
+++ src/HolzShots/Interop/HumanInterop.vb
@@ -1,10 +1,7 @@
-Imports System.Linq
-Imports HolzShots
 Imports HolzShots.Composition
 Imports HolzShots.Input
 Imports HolzShots.Net
 Imports HolzShots.UI.Dialogs
-Imports Microsoft.WindowsAPICodePack.Dialogs
 
 Namespace Interop
     ' TODO: Rename to NotificationManager
@@ -14,18 +11,20 @@ Namespace Interop
 
         Public Shared Sub CopyImageFailed(ex As Exception)
             Show(
-                    GenericErrorTitle,
-                    "Copying failed",
-                    $"Error copying image to clipboard:\n{ex.Message}",
-                    TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error
+                GenericErrorTitle,
+                "Copying failed",
+                $"Error copying image to clipboard:\n{ex.Message}",
+                TaskDialogIcon.Error,
+                TaskDialogButton.OK
             )
         End Sub
         Public Shared Sub RetrievingImageFromClipboardFailed(ex As Exception)
             Show(
-                    GenericErrorTitle,
-                    "Could not fetch image from clipboard",
-                    $"Maybe you know what this means:\n{ex.Message}",
-                    TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error
+                GenericErrorTitle,
+                "Could not fetch image from clipboard",
+                $"Maybe you know what this means:\n{ex.Message}",
+                TaskDialogIcon.Error,
+                TaskDialogButton.OK
             )
         End Sub
         Friend Shared Sub NoAdmin()
@@ -33,7 +32,8 @@ Namespace Interop
                 GenericErrorTitle,
                 "Missing permissions",
                 "We need administrative permissions to change stuff in the explorer context menu.",
-                TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error
+                TaskDialogIcon.Error,
+                TaskDialogButton.OK
             )
         End Sub
 
@@ -42,16 +42,18 @@ Namespace Interop
                 GenericErrorTitle,
                 "Access to folder denied",
                 $"We tried to access the folder\n{directory}\nbut the access was denied.",
-                TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error
+                TaskDialogIcon.Error,
+                TaskDialogButton.OK
             )
         End Sub
         Public Shared Sub PathIsTooLong(directory As String, Optional parent As IWin32Window = Nothing)
             Show(
+                parent,
                 GenericErrorTitle,
                 "Path is too long",
                 $"The path\n{directory}\nis longer than 255 characters. We can only work with path with lesser characters.",
-                TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error,
-                parent
+                TaskDialogIcon.Error,
+                TaskDialogButton.OK
             )
         End Sub
 
@@ -65,7 +67,7 @@ Namespace Interop
         End Sub
 
         Public Shared Sub UploadFailed(result As UploadException)
-            Show("Error Uploading Image", String.Empty, result.Message, TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error)
+            Show("Error Uploading Image", String.Empty, result.Message, TaskDialogIcon.Error, TaskDialogButton.OK)
         End Sub
         Public Shared Sub CopyingFailed(text As String)
             FlyoutNotifier.Notify("Could not copy link :(", $"We could not copy the link to your image to your clipboard.")
@@ -85,7 +87,8 @@ Namespace Interop
                 GenericErrorTitle,
                 "No path specified.",
                 "You did not specify any path that could be used.",
-                TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error
+                TaskDialogIcon.Error,
+                TaskDialogButton.OK
             )
         End Sub
         Shared Sub PathDoesNotExist(path As String)
@@ -93,7 +96,8 @@ Namespace Interop
                 GenericErrorTitle,
                 "The path was not found.",
                 $"The path\n{path}\nyou provided does not exist.",
-                TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error
+                TaskDialogIcon.Error,
+                TaskDialogButton.OK
             )
         End Sub
         Shared Sub InvalidFilePattern(pattern As String)
@@ -102,10 +106,11 @@ Namespace Interop
                 GenericErrorTitle,
                 "No valid naming pattern provided.",
                 $"The file naming pattern you provided is not valid. Therefore, the image was not saved. Please set a valid naming pattern in settings.\nWould you like to turn off automatic saving of your screenshots?",
-                TaskDialogStandardButtons.Yes Or TaskDialogStandardButtons.No,
-                TaskDialogStandardIcon.Error
+                TaskDialogIcon.Error,
+                TaskDialogButton.Yes, TaskDialogButton.No
             )
-            If res = DialogResult.Yes Then
+
+            If res = TaskDialogButton.Yes Then
                 ' UserSettings.Current.SaveImagesToLocalDisk = False
                 ' TODO: Setting settings not supported yet
                 ' Maybe we want to remove the "do you want to turn it off now" feature off
@@ -113,40 +118,48 @@ Namespace Interop
         End Sub
         Shared Sub ErrorSavingImage(ex As Exception, parent As IWin32Window)
             Show(
+                parent,
                 GenericErrorTitle,
                 "There was an error saving your image.",
                 ex.Message,
-                TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error,
-                parent
+                TaskDialogIcon.Error,
+                TaskDialogButton.OK
             )
         End Sub
 
-        Private Shared Function Show(title As String, instructionText As String, text As String, buttons As TaskDialogStandardButtons, icon As TaskDialogStandardIcon, Optional parent As IWin32Window = Nothing) As TaskDialogResult
-            If Not TaskDialog.IsPlatformSupported Then
-                MessageBox.Show(parent, $"{instructionText}\n{text}\n{HS} will now crash because you seem to use an unsupported operating system.", title)
-                Throw New DivideByZeroException()
-            End If
+        Private Shared Function Show(title As String, instructionText As String, text As String, icon As TaskDialogIcon, ParamArray buttons As TaskDialogButton()) As TaskDialogButton
+            Return Show(Nothing, title, instructionText, text, icon, buttons)
+        End Function
 
-            Using diag As New TaskDialog()
-                diag.Caption = title
-                diag.InstructionText = instructionText
-                diag.Text = text
-                diag.Icon = icon
-                diag.StandardButtons = buttons
-                diag.OwnerWindowHandle = If(parent?.Handle, IntPtr.Zero)
-                Return diag.Show()
-            End Using
+        Private Shared Function Show(parent As IWin32Window, title As String, instructionText As String, text As String, icon As TaskDialogIcon, ParamArray buttons As TaskDialogButton()) As TaskDialogButton
+            Dim owner = If(parent?.Handle, IntPtr.Zero)
+
+            Dim buttonCollection As New TaskDialogButtonCollection()
+            For Each b In buttons
+                buttonCollection.Add(b)
+            Next
+
+            Dim page As New TaskDialogPage() With {
+                .Caption = title,
+                .Heading = instructionText,
+                .Text = text,
+                .Icon = icon,
+                .Buttons = buttonCollection
+            }
+
+            Return TaskDialog.ShowDialog(owner, page)
         End Function
 
         Friend Shared Sub ErrorRegisteringHotkeys(exs As IEnumerable(Of HotkeyRegistrationException))
-
             Dim hotkeyList = String.Join("\n", exs.Select(Function(e) "- " + If(e.Hotkey?.ToString(), "<unknown hotkey>")))
 
-            Show(GenericErrorTitle,
+            Show(
+                 GenericErrorTitle,
                  "Error registering hotkeys.",
                  "We could not register the hotkeys you specified. Check if some other application has one or more of these hotkeys already registered or choose different hotkeys." +
                  If(hotkeyList.Length > 0, "\nAffected hotkeys:\n" + hotkeyList, ""),
-                 TaskDialogStandardButtons.Ok, TaskDialogStandardIcon.Error
+                 TaskDialogIcon.Error,
+                 TaskDialogButton.OK
             )
         End Sub
     End Class
