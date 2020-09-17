0ec9f82172466f286e8c85e1a5848c866b709b68
Niklas Mollenhauer <nikeee@outlook.com>
2020-09-17 02:16:38 +0200

    Migrate UploadResultWindow

--- src/HolzShots.Windows/Forms/UploadResultForm.Designer.cs
+++ src/HolzShots.Windows/Forms/UploadResultForm.Designer.cs
@@ -28,30 +28,95 @@ namespace HolzShots.Windows.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.CopyMarkdown = new System.Windows.Forms.Button();
+            this.CopyHTML = new System.Windows.Forms.Button();
+            this.CopyDirectLink = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CloseLabel = new HolzShots.Windows.Forms.ExplorerLinkLabel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            //
+            // 
+            // CopyMarkdown
+            // 
+            this.CopyMarkdown.Location = new System.Drawing.Point(12, 12);
+            this.CopyMarkdown.Name = "CopyMarkdown";
+            this.CopyMarkdown.Size = new System.Drawing.Size(211, 23);
+            this.CopyMarkdown.TabIndex = 1;
+            this.CopyMarkdown.Text = "Copy Markdown";
+            this.CopyMarkdown.UseVisualStyleBackColor = true;
+            this.CopyMarkdown.Click += new System.EventHandler(this.CopyMarkdownClick);
+            // 
+            // CopyHTML
+            // 
+            this.CopyHTML.Location = new System.Drawing.Point(12, 41);
+            this.CopyHTML.Name = "CopyHTML";
+            this.CopyHTML.Size = new System.Drawing.Size(211, 23);
+            this.CopyHTML.TabIndex = 2;
+            this.CopyHTML.Text = "Copy HTML";
+            this.CopyHTML.UseVisualStyleBackColor = true;
+            this.CopyHTML.Click += new System.EventHandler(this.CopyHTMLClick);
+            // 
+            // CopyDirectLink
+            // 
+            this.CopyDirectLink.Location = new System.Drawing.Point(12, 70);
+            this.CopyDirectLink.Name = "CopyDirectLink";
+            this.CopyDirectLink.Size = new System.Drawing.Size(211, 64);
+            this.CopyDirectLink.TabIndex = 0;
+            this.CopyDirectLink.Text = "Copy direct link";
+            this.CopyDirectLink.UseVisualStyleBackColor = true;
+            this.CopyDirectLink.Click += new System.EventHandler(this.CopyDirectLinkClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Menu;
+            this.panel1.Controls.Add(this.CloseLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 149);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 30);
+            this.panel1.TabIndex = 3;
+            // 
+            // CloseLabel
+            // 
+            this.CloseLabel.ActiveLinkColor = System.Drawing.SystemColors.Highlight;
+            this.CloseLabel.AutoSize = true;
+            this.CloseLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.CloseLabel.LinkColor = System.Drawing.SystemColors.HotTrack;
+            this.CloseLabel.Location = new System.Drawing.Point(98, 6);
+            this.CloseLabel.Name = "CloseLabel";
+            this.CloseLabel.Size = new System.Drawing.Size(36, 15);
+            this.CloseLabel.TabIndex = 3;
+            this.CloseLabel.TabStop = true;
+            this.CloseLabel.Text = "Close";
+            this.CloseLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.CloseLabel_LinkClicked);
+            // 
             // UploadResultForm
-            //
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(235, 271);
-            this.ControlBox = false;
-            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
+            this.ClientSize = new System.Drawing.Size(235, 179);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.CopyDirectLink);
+            this.Controls.Add(this.CopyHTML);
+            this.Controls.Add(this.CopyMarkdown);
+            this.MaximumSize = new System.Drawing.Size(251, 195);
+            this.MinimumSize = new System.Drawing.Size(251, 195);
             this.Name = "UploadResultForm";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.TopMost = true;
             this.Load += new System.EventHandler(this.FormLoad);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CopyMarkdown;
+        private System.Windows.Forms.Button CopyHTML;
+        private System.Windows.Forms.Button CopyDirectLink;
+        private System.Windows.Forms.Panel panel1;
+        private ExplorerLinkLabel CloseLabel;
     }
 }
--- src/HolzShots.Windows/Forms/UploadResultForm.cs
+++ src/HolzShots.Windows/Forms/UploadResultForm.cs
@@ -1,20 +1,77 @@
 using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using HolzShots.Net;
 
 namespace HolzShots.Windows.Forms
 {
-    public partial class UploadResultForm : Form
+    public partial class UploadResultForm : FlyoutForm
     {
-        public UploadResultForm()
+        private readonly UploadResult _result;
+        private readonly HSSettings _settingsContext;
+        private readonly FlyoutAnimator _animator;
+        // private readonly string _displayUrl;
+
+        public UploadResultForm(UploadResult result, HSSettings settingsContext)
         {
+            _result = result ?? throw new ArgumentNullException(nameof(result));
+            _settingsContext = settingsContext ?? throw new ArgumentNullException(nameof(settingsContext));
+
             InitializeComponent();
             StartPosition = FormStartPosition.Manual;
+
+            _animator = new FlyoutAnimator(this);
+
+            /*
+            if (!string.IsNullOrEmpty(result.Url))
+                _displayUrl = EnvironmentEx.ShortenViaEllipsisIfNeeded(result.Url, 26);
+            */
+
         }
 
         private void FormLoad(object sender, EventArgs e)
         {
             MaximumSize = Size;
             MinimumSize = Size;
+
+            _animator.AnimateIn(300);
+        }
+
+        void MaybeClose()
+        {
+            if (_settingsContext.AutoCloseLinkViewer)
+                _ = CloseFlyout();
+        }
+
+        async Task CloseFlyout()
+        {
+            await _animator.AnimateOut(150).ConfigureAwait(true);
+            Close();
+        }
+
+        private void CloseLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) => _ = CloseFlyout();
+        private void CopyAndMaybeClose(string value)
+        {
+            if (SetText(value))
+                MaybeClose();
+        }
+
+        private void CopyMarkdownClick(object sender, EventArgs e) => CopyAndMaybeClose($"![Screenshot]({_result.Url})");
+        private void CopyHTMLClick(object sender, EventArgs e) => CopyAndMaybeClose($"<img src=\"{_result.Url}\">");
+        private void CopyDirectLinkClick(object sender, EventArgs e) => CopyAndMaybeClose(_result.Url);
+
+        // TODO: Replace with ClipboardEx once it's available
+        private static bool SetText(string value)
+        {
+            try
+            {
+                Clipboard.SetText(value);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
