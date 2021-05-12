16380cf0f0d27c16c28020cc936bdaaf449d2d5e
Niklas Mollenhauer <nikeee@outlook.com>
2021-05-12 23:42:48 +0200

    Fix warning

--- src/WindowsAPICodePack.ShellModified/Taskbar/TaskbarWindowManager.cs
+++ src/WindowsAPICodePack.ShellModified/Taskbar/TaskbarWindowManager.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Copyright (c) Microsoft Corporation.  All rights reserved.
 
 using Microsoft.WindowsAPICodePack.Shell;
 using Microsoft.WindowsAPICodePack.Resources;
@@ -366,7 +366,7 @@ namespace Microsoft.WindowsAPICodePack.Taskbar
                     // Only set the peek bitmap if it's not null. 
                     // If it's null (either we didn't get the bitmap or size was 0),
                     // let DWM handle it
-                    if (hBitmap != null)
+                    if (hBitmap != IntPtr.Zero)
                     {
                         TabbedThumbnailNativeMethods.SetPeekBitmap(taskbarWindow.WindowToTellTaskbarAbout, hBitmap, taskbarWindow.TabbedThumbnail.DisplayFrameAroundBitmap);
                     }
