38bf8b583943bf61168190d2b7ebf28b33b6127d
Niklas Mollenhauer <nikeee@outlook.com>
2021-06-05 05:08:33 +0200

    Fix nullability of exception

--- src/HolzShots.Core/Net/UploadException.cs
+++ src/HolzShots.Core/Net/UploadException.cs
@@ -7,22 +7,15 @@ namespace HolzShots.Net
     {
         public UploadException() : this("An error during upload occurred.") { }
         public UploadException(string message) : this(message, null) { }
-        public UploadException(string message, Exception innerException) : base(message, innerException) { }
+        public UploadException(string message, Exception? innerException) : base(message, innerException) { }
     }
 
     [Serializable]
     public class UploadCanceledException : UploadException
     {
-        public UploadCanceledException(OperationCanceledException innerException)
-            : base("Upload was canceled", innerException) { }
-
-        public UploadCanceledException()
-            : this("Upload was canceled") { }
-
-        public UploadCanceledException(string message)
-            : base(message) { }
-
-        public UploadCanceledException(string message, Exception innerException)
-            : base(message, innerException) { }
+        public UploadCanceledException(OperationCanceledException innerException) : base("Upload was canceled", innerException) { }
+        public UploadCanceledException() : this("Upload was canceled") { }
+        public UploadCanceledException(string message) : base(message) { }
+        public UploadCanceledException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
