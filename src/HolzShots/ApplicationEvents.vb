dd6a00d03792293633f3aac0de8e3e686c7dab83
Niklas Mollenhauer <nikeee@outlook.com>
2021-09-05 22:53:49 +0200

    Remove applicationevents

--- src/HolzShots/ApplicationEvents.vb
+++
@@ -1,7 +0,0 @@
-Imports System.Globalization
-
-Namespace My
-    Partial Friend Class MyApplication
-        Friend ReadOnly TheCulture As New CultureInfo("en-US", False)
-    End Class
-End Namespace
