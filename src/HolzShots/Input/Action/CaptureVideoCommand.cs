933c6baf328b39c113aca49bc1883b29a93b873f
Niklas Mollenhauer <nikeee@outlook.com>
2021-10-03 20:42:28 +0200

    Move function

--- src/HolzShots/Input/Action/CaptureVideoCommand.cs
+++ src/HolzShots/Input/Action/CaptureVideoCommand.cs
@@ -18,6 +18,90 @@ namespace HolzShots.Input.Actions
         private static CancellationTokenSource? _currentRecordingCts = null;
         private static bool _throwAwayResult = false;
 
+        public async Task Invoke(IReadOnlyDictionary<string, string> parameters, HSSettings settingsContext)
+        {
+            // TODO: If it is the first time the user invokes this command, we want to tell him that he can stop recording by pressing the same button again
+
+            if (IsScreenRecorderRunning())
+            {
+                // We allow only one instance running. If the user invokes this task another time, he likely wanted to stop recording.
+                // TODO: Add UI and trigger cancellation manually via mouse
+                StopCurrentScreenRecorder(false);
+                return;
+            }
+
+            var recording = await PerformScreenRecording(settingsContext);
+            if (recording == null)
+                return; // User likely cancelled recording
+
+
+            // Every actionAfterCapture depends on the file in some way
+            // Sometimes, it will be saved to a specified path
+            // In that case, we want to first move the file to the target destination and then perform the action with that new path
+            // Otherwise, we use the file in the temp dir
+
+            if (settingsContext.SaveToLocalDisk)
+            {
+                // TODO: Support file patterns like with screenshots
+                // TODO: Unify screenshot and video recording saving stuff
+
+                // This is a temporary solution to get an MVP working
+
+                var destDir = settingsContext.ExpandedSavePath;
+                if (!Directory.Exists(destDir))
+                    IO.HolzShotsPaths.EnsureDirectory(destDir);
+
+
+                var timestamp = recording.StartTime.ToString(System.Globalization.DateTimeFormatInfo.InvariantInfo.SortableDateTimePattern);
+                var targetFileName = timestamp.Replace(':', '-') + "-" + Path.GetFileName(recording.FilePath);
+
+                var fullTargetFilePath = Path.Combine(destDir, targetFileName);
+
+                File.Move(recording.FilePath, fullTargetFilePath);
+
+                recording = recording with { FilePath = fullTargetFilePath };
+            }
+
+
+            switch (settingsContext.ActionAfterVideoCapture)
+            {
+                case VideoCaptureHandlingAction.Upload:
+                    return; // TODO
+                case VideoCaptureHandlingAction.CopyFile:
+                    {
+                        var success = ClipboardEx.SetFiles(recording.FilePath);
+                        if (settingsContext.ShowCopyConfirmation)
+                        {
+                            if (success)
+                                NotificationManager.ShowFileCopyConfirmation();
+                            else
+                                NotificationManager.CopyingFileFailed();
+                        }
+                    }
+                    break;
+                case VideoCaptureHandlingAction.CopyFilePath:
+                    {
+                        var success = ClipboardEx.SetText(recording.FilePath);
+                        if (settingsContext.ShowCopyConfirmation)
+                        {
+                            if (success)
+                                NotificationManager.ShowFilePathCopyConfirmation();
+                            else
+                                NotificationManager.CopyingFilePathFailed();
+                        }
+                    }
+                    break;
+                case VideoCaptureHandlingAction.ShowInExplorer:
+                    IO.HolzShotsPaths.OpenSelectedFileInExplorer(recording.FilePath);
+                    return;
+                case VideoCaptureHandlingAction.OpenInDefaultApp:
+                    Process.Start(recording.FilePath);
+                    return;
+                case VideoCaptureHandlingAction.None: return;
+                default: throw new ArgumentException("Unhandled VideoCaptureHandlingAction: " + settingsContext.ActionAfterVideoCapture);
+            }
+        }
+
         private async Task<ScreenRecording?> PerformScreenRecording(HSSettings settingsContext)
         {
             _currentRecordingCts = new CancellationTokenSource();
@@ -72,91 +156,6 @@ namespace HolzShots.Input.Actions
             {
                 _currentRecordingCts = null;
                 _throwAwayResult = false;
-            }
-        }
-
-        public async Task Invoke(IReadOnlyDictionary<string, string> parameters, HSSettings settingsContext)
-        {
-            // TODO: If it is the first time the user invokes this command, we want to tell him that he can stop recording by pressing the same button again
-
-            if (IsScreenRecorderRunning())
-            {
-                // We allow only one instance running. If the user invokes this task another time, he likely wanted to stop recording.
-                // TODO: Add UI and trigger cancellation manually via mouse
-                StopCurrentScreenRecorder(false);
-                return;
-            }
-
-            var recording = await PerformScreenRecording(settingsContext);
-            if (recording == null)
-                return; // User likely cancelled recording
-
-
-            // Every actionAfterCapture depends on the file in some way
-            // Sometimes, it will be saved to a specified path
-            // In that case, we want to first move the file to the target destination and then perform the action with that new path
-            // Otherwise, we use the file in the temp dir
-
-            if (settingsContext.SaveToLocalDisk)
-            {
-                // TODO: Support file patterns like with screenshots
-                // TODO: Unify screenshot and video recording saving stuff
-
-                // This is a temporary solution to get an MVP working
-
-                var destDir = settingsContext.ExpandedSavePath;
-                if (!Directory.Exists(destDir))
-                    IO.HolzShotsPaths.EnsureDirectory(destDir);
-
-
-                var timestamp = recording.StartTime.ToString(System.Globalization.DateTimeFormatInfo.InvariantInfo.SortableDateTimePattern);
-                var targetFileName = timestamp.Replace(':', '-') + "-" + Path.GetFileName(recording.FilePath);
-
-                var fullTargetFilePath = Path.Combine(destDir, targetFileName);
-
-                File.Move(recording.FilePath, fullTargetFilePath);
-
-                recording = recording with { FilePath = fullTargetFilePath };
-            }
-
-
-
-            switch (settingsContext.ActionAfterVideoCapture)
-            {
-                case VideoCaptureHandlingAction.Upload:
-                    return; // TODO
-                case VideoCaptureHandlingAction.CopyFile:
-                    {
-                        var success = ClipboardEx.SetFiles(recording.FilePath);
-                        if (settingsContext.ShowCopyConfirmation)
-                        {
-                            if (success)
-                                NotificationManager.ShowFileCopyConfirmation();
-                            else
-                                NotificationManager.CopyingFileFailed();
-                        }
-                    }
-                    break;
-                case VideoCaptureHandlingAction.CopyFilePath:
-                    {
-                        var success = ClipboardEx.SetText(recording.FilePath);
-                        if (settingsContext.ShowCopyConfirmation)
-                        {
-                            if (success)
-                                NotificationManager.ShowFilePathCopyConfirmation();
-                            else
-                                NotificationManager.CopyingFilePathFailed();
-                        }
-                    }
-                    break;
-                case VideoCaptureHandlingAction.ShowInExplorer:
-                    IO.HolzShotsPaths.OpenSelectedFileInExplorer(recording.FilePath);
-                    return;
-                case VideoCaptureHandlingAction.OpenInDefaultApp:
-                    Process.Start(recording.FilePath);
-                    return;
-                case VideoCaptureHandlingAction.None: return;
-                default: throw new ArgumentException("Unhandled VideoCaptureHandlingAction: " + settingsContext.ActionAfterVideoCapture);
             }
         }
 
