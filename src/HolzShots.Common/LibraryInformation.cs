fdc8c6e0e279b7ca19e551ea983590783ba5fe48
Niklas Mollenhauer <nikeee@outlook.com>
2023-06-14 22:55:19 +0200

    Inline vars

--- src/HolzShots.Common/LibraryInformation.cs
+++ src/HolzShots.Common/LibraryInformation.cs
@@ -5,14 +5,10 @@ public static class LibraryInformation
 {
     public const string Name = ThisAssembly.Project.RootNamespace;
 
-    // 1.0.0-beta.2+deadbeef
     // These info is filled by build script
-    public const string VersionFormal = "%VERSION%"; // 1.0.0
-    private const string ReleaseSuffix = "%RELEASE-SUFFIX%"; // -beta
-    public const string CommitsSinceTag = "%COMMITS-SINCE-TAG%"; // .2
-    public const string CommitId = "%COMMIT-ID%"; // +deadbeef
+    // 1.0.0-beta.2+deadbeef
+    public const string SemanticVersion = "%VERSION%" + "%RELEASE-SUFFIX%" + "%COMMITS-SINCE-TAG%" + "%COMMIT-ID%";
     public static readonly DateTime VersionDate = DateTime.ParseExact(ThisAssembly.Constants.BuildDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-    public const string SemanticVersion = VersionFormal + ReleaseSuffix + CommitsSinceTag + CommitId;
 
     public const string SiteUrl = ThisAssembly.Constants.Website;
     public const string SourceUrl = ThisAssembly.Metadata.RepositoryUrl;
