1b9ad5b6a122f6b471b4bf4c87fec94be1a3164e
Niklas Mollenhauer <nikeee@outlook.com>
2024-01-15 00:55:29 +0100

    Add actual test assertions

--- src/HolzShots.Core.Tests/Drawing/ComputationTest.cs
+++ src/HolzShots.Core.Tests/Drawing/ComputationTest.cs
@@ -1,6 +1,7 @@
 using System.Drawing;
 using System.IO;
 using HolzShots.Core.Tests;
+using HolzShots.Drawing;
 using Xunit;
 
 namespace HolzShots.Tests.Drawing
@@ -18,6 +19,25 @@ namespace HolzShots.Tests.Drawing
             var w = new Bitmap(white);
             var b = new Bitmap(black);
             var e = new Bitmap(expected);
+
+            // Sanity check for input data
+            Assert.Equal(w.Width, b.Width);
+            Assert.Equal(w.Height, b.Height);
+            Assert.Equal(e.Height, b.Height);
+
+            var actual = new Bitmap(w.Width, w.Height);
+            Computation.ComputeAlphaChannel(w, b, ref actual);
+
+            Assert.Equal(e.Width, actual.Width);
+            Assert.Equal(e.Height, actual.Height);
+
+            for (var x = 0; x < w.Width; ++x)
+            {
+                for (var y = 0; y < w.Height; ++y)
+                {
+                    Assert.Equal(e.GetPixel(x, y), actual.GetPixel(x, y));
+                }
+            }
         }
     }
 }
