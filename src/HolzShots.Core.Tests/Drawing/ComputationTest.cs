0d0fc1d02eb2cc6f9b3743e23a0939d6f69a0cc5
Niklas Mollenhauer <nikeee@outlook.com>
2024-01-15 01:33:35 +0100

    Use file-scoped namespace

--- src/HolzShots.Core.Tests/Drawing/ComputationTest.cs
+++ src/HolzShots.Core.Tests/Drawing/ComputationTest.cs
@@ -4,67 +4,79 @@ using HolzShots.Core.Tests;
 using HolzShots.Drawing;
 using Xunit;
 
-namespace HolzShots.Tests.Drawing
+namespace HolzShots.Tests.Drawing;
+
+public class ComputationTest
 {
-    public class ComputationTest
+    [Theory]
+    [FileStreamContentData("Files/0-white.png", "Files/0-black.png", "Files/0-expected.png")]
+    public void ComputeAlphaChannel(Stream whiteData, Stream blackData, Stream expectedData)
     {
-        [Theory]
-        [FileStreamContentData("Files/0-white.png", "Files/0-black.png", "Files/0-expected.png")]
-        public void ComputeAlphaChannel(Stream whiteData, Stream blackData, Stream expectedData)
-        {
-            Assert.True(whiteData.Length > 0);
-            Assert.True(blackData.Length > 0);
-            Assert.True(expectedData.Length > 0);
+        Assert.True(whiteData.Length > 0);
+        Assert.True(blackData.Length > 0);
+        Assert.True(expectedData.Length > 0);
 
-            var white = new Bitmap(whiteData);
-            var black = new Bitmap(blackData);
-            var expected = new Bitmap(expectedData);
+        var white = new Bitmap(whiteData);
+        var black = new Bitmap(blackData);
+        var expected = new Bitmap(expectedData);
 
-            // Sanity check for input data
-            Assert.Equal(white.Width, black.Width);
-            Assert.Equal(white.Height, black.Height);
-            Assert.Equal(expected.Height, black.Height);
+        // Sanity check for input data
+        Assert.Equal(white.Width, black.Width);
+        Assert.Equal(white.Height, black.Height);
+        Assert.Equal(expected.Height, black.Height);
 
-            var actual = new Bitmap(white.Width, white.Height);
+        var actual = new Bitmap(white.Width, white.Height);
+
+        var start = DateTime.Now;
+        for (int i = 0; i < 1000; i++)
             Computation.ComputeAlphaChannel(white, black, ref actual);
+        var end = DateTime.Now;
+        Console.WriteLine(end - start);
 
-            AssertImage(expected, actual);
-        }
+        Computation.ComputeAlphaChannel(white, black, ref actual);
 
-        [Theory]
-        [FileStreamContentData("Files/0-white.png", "Files/0-black.png", "Files/0-expected.png")]
-        public void ComputeAlphaChannel2(Stream whiteData, Stream blackData, Stream expectedData)
-        {
-            Assert.True(whiteData.Length > 0);
-            Assert.True(blackData.Length > 0);
-            Assert.True(expectedData.Length > 0);
+        AssertImage(expected, actual);
+    }
 
-            var white = new Bitmap(whiteData);
-            var black = new Bitmap(blackData);
-            var expected = new Bitmap(expectedData);
+    [Theory]
+    [FileStreamContentData("Files/0-white.png", "Files/0-black.png", "Files/0-expected.png")]
+    public void ComputeAlphaChannel2(Stream whiteData, Stream blackData, Stream expectedData)
+    {
+        Assert.True(whiteData.Length > 0);
+        Assert.True(blackData.Length > 0);
+        Assert.True(expectedData.Length > 0);
 
-            // Sanity check for input data
-            Assert.Equal(white.Width, black.Width);
-            Assert.Equal(white.Height, black.Height);
-            Assert.Equal(expected.Height, black.Height);
+        var white = new Bitmap(whiteData);
+        var black = new Bitmap(blackData);
+        var expected = new Bitmap(expectedData);
 
-            var actual = new Bitmap(white.Width, white.Height);
+        // Sanity check for input data
+        Assert.Equal(white.Width, black.Width);
+        Assert.Equal(white.Height, black.Height);
+        Assert.Equal(expected.Height, black.Height);
+
+        var actual = new Bitmap(white.Width, white.Height);
+
+        var start = DateTime.Now;
+        for(int i = 0; i < 1000;  i++)
             Computation.ComputeAlphaChannel2(white, black, ref actual);
+        var end = DateTime.Now;
+        Console.WriteLine(end - start);
 
-            AssertImage(expected, actual);
-        }
 
-        private static void AssertImage(Bitmap expected, Bitmap actual)
+        AssertImage(expected, actual);
+    }
+
+    private static void AssertImage(Bitmap expected, Bitmap actual)
+    {
+        Assert.Equal(expected.Width, actual.Width);
+        Assert.Equal(expected.Height, actual.Height);
+
+        for (var x = 0; x < expected.Width; ++x)
         {
-            Assert.Equal(expected.Width, actual.Width);
-            Assert.Equal(expected.Height, actual.Height);
-
-            for (var x = 0; x < expected.Width; ++x)
+            for (var y = 0; y < expected.Height; ++y)
             {
-                for (var y = 0; y < expected.Height; ++y)
-                {
-                    Assert.Equal(expected.GetPixel(x, y), actual.GetPixel(x, y));
-                }
+                Assert.Equal(expected.GetPixel(x, y), actual.GetPixel(x, y));
             }
         }
     }
