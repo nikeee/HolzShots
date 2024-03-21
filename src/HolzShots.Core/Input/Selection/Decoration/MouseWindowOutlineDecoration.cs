baccb73be2e1e03c33fc81453cc9a0e3febf07ff
Niklas Mollenhauer <nikeee@outlook.com>
2024-03-21 20:13:55 +0100

    Add background to window label marker

--- src/HolzShots.Core/Input/Selection/Decoration/MouseWindowOutlineDecoration.cs
+++ src/HolzShots.Core/Input/Selection/Decoration/MouseWindowOutlineDecoration.cs
@@ -8,7 +8,9 @@ class MouseWindowOutlineDecoration : IStateDecoration<InitialState>
     private const string FontName = "Consolas";
     private const float FontSize = 14.0f;
     private D2DColor FontColor = D2DColor.WhiteSmoke;
+    private D2DColor BackgroundColor = new(1f, 0.2f, 0.2f, 0.2f);
     private D2DColor OutlineColor = D2DColor.White;
+    private readonly D2DSize _placeSize = new(1000, 1000);
 
     public static MouseWindowOutlineDecoration ForContext(D2DGraphics g, DateTime now) => new();
 
@@ -25,8 +27,50 @@ class MouseWindowOutlineDecoration : IStateDecoration<InitialState>
 
         var windowTitle = state.Title;
         if (windowTitle is not null)
-            g.DrawTextCenter(windowTitle, FontColor, FontName, FontSize, rect);
+        {
+            using var backgroundBush = g.Device.CreateSolidColorBrush(BackgroundColor);
+
+            var textSize = g.MeasureText(windowTitle, FontName, FontSize, _placeSize);
+
+            var padding = new Size(10, 4);
+
+            var backgroundRectangle = new D2DRect(
+                rect.X + (int)(rect.Width / 2) - (int)(textSize.Width / 2),
+                rect.Y + (int)(rect.Height / 2) - (int)(textSize.Height / 2),
+                textSize.Width,
+                textSize.Height
+            );
+            backgroundRectangle.Inflate(padding);
+
+            g.FillRectangle(backgroundRectangle, backgroundBush);
+            g.DrawTextCenter(windowTitle, FontColor, FontName, FontSize, backgroundRectangle);
+        }
     }
 
     public void Dispose() { }
+}
+
+
+static class D2DRectExtensions
+{
+    /// <summary>Like System.Drawing.Rectangle.Inflate(). Maintains rectangle's center. </summary>
+    public static void Inflate(this ref D2DRect rectangle, D2DSize size) => Inflate(ref rectangle, size.Width, size.Height);
+    /// <summary>Like System.Drawing.Rectangle.Inflate(). Maintains rectangle's center. </summary>
+    public static void Inflate(this ref D2DRect rectangle, float width, float height)
+    {
+        rectangle.X -= width;
+        rectangle.Y -= height;
+        rectangle.Width += width * 2;
+        rectangle.Height += height * 2;
+    }
+    /// <summary>Like System.Drawing.Rectangle.Inflate(). Maintains rectangle's center. </summary>
+    public static void Inflate(this ref D2DRect rectangle, Size size) => Inflate(ref rectangle, size.Width, size.Height);
+    /// <summary>Like System.Drawing.Rectangle.Inflate(). Maintains rectangle's center. </summary>
+    public static void Inflate(this ref D2DRect rectangle, int width, int height)
+    {
+        rectangle.X -= width;
+        rectangle.Y -= height;
+        rectangle.Width += width * 2;
+        rectangle.Height += height * 2;
+    }
 }
