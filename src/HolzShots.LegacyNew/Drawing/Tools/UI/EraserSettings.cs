8eda7112440b2098b120e1328c1545c5f7f873e6
Niklas Mollenhauer <nikeee@outlook.com>
2024-10-26 06:25:40 +0200

    Add new project for new controls

---
+++ src/HolzShots.LegacyNew/Drawing/Tools/UI/EraserSettings.cs
@@ -0,0 +1,3 @@
+namespace HolzShots.Drawing.Tools.UI;
+
+public readonly record struct EraserSettings(int Diameter);
---
+++ src/HolzShots.LegacyNew/Drawing/Tools/UI/EraserSettingsControl.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel;
+
+namespace HolzShots.Drawing.Tools.UI;
+
+[DefaultBindingProperty("Settings")]
+public partial class EraserSettingsControl : UserControl
+{
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    public EraserSettings Settings
+    {
+        get => new(EraserDiameterTrackBar.Value);
+        set => EraserDiameterTrackBar.Value = value.Diameter;
+    }
+
+    public EraserSettingsControl()
+    {
+        InitializeComponent();
+    }
+}
