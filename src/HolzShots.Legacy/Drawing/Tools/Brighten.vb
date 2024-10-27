f41f8269787ba300525260c5d913febf5b97c568
Niklas Mollenhauer <nikeee@outlook.com>
2024-10-27 21:47:57 +0100

    Use Min

--- src/HolzShots.Legacy/Drawing/Tools/Brighten.vb
+++ src/HolzShots.Legacy/Drawing/Tools/Brighten.vb
@@ -8,18 +8,26 @@ Namespace Drawing.Tools
         Public Overrides ReadOnly Property ToolType As PaintPanel.ShotEditorTool = PaintPanel.ShotEditorTool.Brighten
 
         Public Overrides Sub RenderFinalImage(ByRef rawImage As Image, sender As PaintPanel)
-            Dim startPoint As New Point(Math.Min(BeginCoordinates.X, EndCoordinates.X), Math.Min(BeginCoordinates.Y, EndCoordinates.Y))
+            Dim rct As New Rectangle(
+                Math.Min(BeginCoordinates.X, EndCoordinates.X),
+                Math.Min(BeginCoordinates.Y, EndCoordinates.Y),
+                Math.Abs(BeginCoordinates.X - EndCoordinates.X),
+                Math.Abs(BeginCoordinates.Y - EndCoordinates.Y)
+            )
 
-            Dim rct As New Rectangle(startPoint.X, startPoint.Y, Math.Abs(BeginCoordinates.X - EndCoordinates.X), Math.Abs(BeginCoordinates.Y - EndCoordinates.Y))
             Using gr = Graphics.FromImage(rawImage)
                 gr.FillRectangle(New SolidBrush(sender.BrightenColor), rct)
             End Using
         End Sub
 
         Public Overrides Sub RenderPreview(rawImage As Image, g As Graphics, sender As PaintPanel)
+            Dim rct As New Rectangle(
+                Math.Min(BeginCoordinates.X, EndCoordinates.X),
+                Math.Min(BeginCoordinates.Y, EndCoordinates.Y),
+                Math.Abs(BeginCoordinates.X - EndCoordinates.X),
+                Math.Abs(BeginCoordinates.Y - EndCoordinates.Y)
+            )
 
-            Dim thePoint As New Point(If(BeginCoordinates.X > EndCoordinates.X, EndCoordinates.X, BeginCoordinates.X), If(BeginCoordinates.Y > EndCoordinates.Y, EndCoordinates.Y, BeginCoordinates.Y))
-            Dim rct As New Rectangle(thePoint.X, thePoint.Y, Math.Abs(BeginCoordinates.X - EndCoordinates.X), Math.Abs(BeginCoordinates.Y - EndCoordinates.Y))
             Using s As New SolidBrush(sender.BrightenColor)
                 g.FillRectangle(s, rct)
             End Using
