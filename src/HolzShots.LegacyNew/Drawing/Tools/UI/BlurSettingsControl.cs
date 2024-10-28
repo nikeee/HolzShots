d9c91e66f74a5bb55345f938fd2186c07f4651b7
Niklas Mollenhauer <nikeee@outlook.com>
2024-10-28 01:42:44 +0100

    Add BlurSettingsControl

---
+++ src/HolzShots.LegacyNew/Drawing/Tools/UI/BlurSettingsControl.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel;
+
+namespace HolzShots.Drawing.Tools.UI;
+
+[DefaultBindingProperty("Settings")]
+public partial class BlurSettingsControl : UserControl, ISettingsControl<BlurSettings>
+{
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    public BlurSettings Settings
+    {
+        get => new(BlurDiameterTrackBar.Value);
+        set
+        {
+            BlurDiameterTrackBar.Value = value.Diameter;
+        }
+    }
+
+    public BlurSettingsControl(BlurSettings initialSettings)
+    {
+        InitializeComponent();
+        Settings = initialSettings;
+        BlurDiameterTrackBar.ValueChanged += (_, _) =>
+        {
+            BlurDiameterTrackBarLabel.Text = $"{BlurDiameterTrackBar.Value}px";
+        };
+    }
+}
+
+public class BlurSettings(int diameter) : ToolSettingsBase
+{
+    public int Diameter { get; init; } = diameter;
+}
